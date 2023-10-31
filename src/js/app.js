eea6988e6697bd6bc97090fc65a0380a1784607b
Ivan Izvekov <Ivan.izvekov02@mail.ru>
2023-10-31 14:39:24 +0700

    addYmapsWidget

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+//import Maps from "./modules/Maps";
 import FindForm from "./modules/FindForm";
 
 const isMobile = document.documentElement.clientWidth < 768;
@@ -24,8 +25,36 @@ function isWebp() {
 isWebp();
 
 document.addEventListener('DOMContentLoaded', (event) => {
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    });
     const findFormNode = document.querySelector('.find__input-group');
     if(findFormNode) {
         new FindForm(findFormNode);
     }
 })
+function mapView() {
+    var node = document.getElementById('complexStreet').innerHTML;
+
+    var myGeocoder = ymaps.geocode("Новосибирск " + node);
+
+    myGeocoder.then(
+        function (res) {
+            var coords = res.geoObjects.get(0).geometry.getCoordinates();
+            
+            var myMap = new ymaps.Map("map", {
+                center: coords,
+                zoom: 16
+            });
+            var myGeoObject = new ymaps.GeoObject({
+                geometry: {
+                    type: "Point",
+                    coordinates: coords
+                }
+            })
+            myMap.geoObjects.add(myGeoObject);
+        })
+}
+
+ymaps.ready(mapView);
---
+++ src/js/modules/Maps.js
@@ -0,0 +1,18 @@
+// class Maps {
+//     constructor(root) {
+//         this.root = root;
+//         this.myGeocoder = ymaps.geocode("Новосибирск");
+//         this.init();
+//     }
+
+//     init() {
+//         ymaps.ready(mapView);
+//     }
+// }
+
+// function mapView() {
+//     var myMap = new ymaps.Map("map", {
+//         center: [55.76, 37.64],
+//         zoom: 7
+//     });
+// }
